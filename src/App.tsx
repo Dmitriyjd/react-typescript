d736824ebd2df18aebfaa2326782202e671f02c4
Dmitriyjd <dimon05091996abcd@mail.ru>
2019-08-10 20:57:15 +0300

    Create skeleton

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Home from './pages/Home/component'
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <Home/>
+  );
+};
+
+export default App;
